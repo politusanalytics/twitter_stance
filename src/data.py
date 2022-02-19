927d5879106df529301a0f6177947a35b39a63e7
Osman Mutlu <osmanmutlu92@gmail.com>
2022-02-19 14:01:02 +0300

    First commit

---
+++ src/data.py
@@ -0,0 +1,64 @@
+import json
+import torch
+# import pandas as pd
+
+class TransformersData(torch.utils.data.Dataset):
+    def __init__(self, examples, label_list, tokenizer, max_seq_length=512, has_token_type_ids=False, with_label=True):
+        self.examples = examples
+        self.label_list = label_list
+        label_map = {}
+        for (i, label) in enumerate(label_list):
+            label_map[label] = i
+
+        self.label_map = label_map
+        self.max_seq_length = max_seq_length
+        self.tokenizer = tokenizer
+        self.with_label = with_label
+        self.has_token_type_ids = has_token_type_ids
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, idx):
+        ex = self.examples[idx]
+        encoded_input = self.tokenizer(ex[0], padding="max_length", truncation=True, max_length=self.max_seq_length)
+        input_ids = torch.tensor(encoded_input["input_ids"], dtype=torch.long)
+        input_mask = torch.tensor(encoded_input["attention_mask"], dtype=torch.long)
+        if self.has_token_type_ids:
+            token_type_ids = torch.tensor(encoded_input["token_type_ids"], dtype=torch.long)
+
+        if self.with_label:
+            if len(self.label_list) == 2:
+                label_ids = torch.FloatTensor([self.label_map[ex[1]]])
+            else:
+                label_ids = torch.tensor(self.label_map[ex[1]], dtype=torch.long)
+
+            if self.has_token_type_ids:
+                return input_ids, input_mask, token_type_ids, label_ids
+            else:
+                return input_ids, input_mask, label_ids
+
+        if self.has_token_type_ids:
+            return input_ids, input_mask, token_type_ids
+        else:
+            return input_ids, input_mask
+
+
+def get_examples(filename, with_label=True):
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.read().splitlines()
+
+    examples = []
+    for (i, line) in enumerate(lines):
+        line = json.loads(line)
+        text = str(line["text"])
+        if with_label:
+            label = str(int(line["label"]))
+            examples.append([text, label])
+        else:
+            examples.append([text])
+
+    # examples = pd.read_json(filename, orient="records", lines=True)
+    # examples = [[a[1]] for a in examples.values.tolist()]
+
+    return examples
---
+++ src/train.py
@@ -0,0 +1,261 @@
+from sklearn.metrics import precision_recall_fscore_support, matthews_corrcoef
+import torch
+from transformers import AutoModel, AutoTokenizer, get_linear_schedule_with_warmup
+from data import get_examples, TransformersData
+from torch.utils.data import DataLoader
+import numpy as np
+import time
+import random
+from tqdm import tqdm
+import json
+import sys
+# import pandas as pd
+
+# INPUTS
+pretrained_transformers_model = sys.argv[1] # For example: "xlm-roberta-base"
+seed = int(sys.argv[2])
+max_seq_length = int(sys.argv[3]) # max length of a document (in tokens)
+batch_size = int(sys.argv[4])
+dev_ratio = float(sys.argv[5])
+
+# MUST SET THESE VALUES
+repo_path = "/path/to/this/repo"
+train_filename = repo_path + "/data/train_examples.json" # sys.argv[1]
+test_filename = repo_path + "/data/test_examples.json"
+# test_filename = repo_path + "/data/examples_to_be_predicted.json"
+label_list = ["category1", "category2", "category3"]
+only_test = False # Only perform testing
+predict = False # Predict instead of testing
+has_token_type_ids = False
+
+# SETTINGS
+learning_rate = 2e-5
+dev_metric = "f1_macro"
+num_epochs = 30
+dev_set_splitting = "random" # random, or any filename
+use_gpu = True
+device_ids = [0, 1, 2, 3, 4, 5, 6, 7]
+
+
+if use_gpu and torch.cuda.is_available():
+    device = torch.device("cuda:%d"%(device_ids[0]))
+else:
+    device = torch.device("cpu")
+
+np.random.seed(seed)
+torch.manual_seed(seed)
+if device.type == "cuda":
+    torch.cuda.manual_seed_all(seed)
+
+tokenizer = None
+model_path = "{}_{}_{}_{:.2f}_{}.pt".format(pretrained_transformers_model.replace("/", "_"), max_seq_length, batch_size, dev_ratio, seed)
+criterion = torch.nn.BCEWithLogitsLoss() if len(label_list) == 2 else torch.nn.CrossEntropyLoss(ignore_index=-1)
+
+def test_model(encoder, classifier, dataloader):
+    all_preds = []
+    all_label_ids = []
+    eval_loss = 0
+    nb_eval_steps = 0
+    for val_step, batch in enumerate(dataloader):
+        if has_token_type_ids:
+            input_ids, input_mask, token_type_ids, label_ids = tuple(t.to(device) for t in batch)
+            embeddings = encoder(input_ids, attention_mask=input_mask, token_type_ids=token_type_ids)[1]
+        else:
+            input_ids, input_mask, label_ids = tuple(t.to(device) for t in batch)
+            embeddings = encoder(input_ids, attention_mask=input_mask)[1]
+
+        with torch.no_grad():
+            out = classifier(embeddings)
+            tmp_eval_loss = criterion(out, label_ids.view(-1))
+
+        eval_loss += tmp_eval_loss.mean().item()
+
+        if len(label_list) == 2:
+            curr_preds = torch.sigmoid(out).detach().cpu().numpy().flatten()
+            curr_preds = [int(x >= 0.5) for x in curr_preds]
+            all_preds += curr_preds
+        else:
+            out = out.detach().cpu().numpy()
+            all_preds += np.argmax(out, axis=1).tolist()
+
+        label_ids = label_ids.to('cpu').numpy().flatten().tolist()
+        all_label_ids += label_ids
+
+        nb_eval_steps += 1
+
+    precision, recall, f1, _ = precision_recall_fscore_support(all_label_ids, all_preds, average="macro", labels=list(range(0,len(label_list))))
+    mcc = matthews_corrcoef(all_preds, all_label_ids)
+    eval_loss /= nb_eval_steps
+    result = {"precision_macro": precision,
+              "recall_macro": recall,
+              "f1_macro": f1,
+              "mcc": mcc}
+
+    return result, eval_loss
+
+
+def model_predict(encoder, classifier, dataloader):
+    all_preds = []
+    for val_step, batch in enumerate(dataloader):
+        if has_token_type_ids:
+            input_ids, input_mask, token_type_ids = tuple(t.to(device) for t in batch)
+            embeddings = encoder(input_ids, attention_mask=input_mask, token_type_ids=token_type_ids)[1]
+        else:
+            input_ids, input_mask = tuple(t.to(device) for t in batch)
+            embeddings = encoder(input_ids, attention_mask=input_mask)[1]
+
+        with torch.no_grad():
+            out = classifier(embeddings)
+
+        if len(label_list) == 2:
+            curr_preds = torch.sigmoid(out).detach().cpu().numpy().flatten()
+            curr_preds = [int(x >= 0.5) for x in curr_preds]
+            all_preds += curr_preds
+        else:
+            out = out.detach().cpu().numpy()
+            all_preds += np.argmax(out, axis=1).tolist()
+
+    return all_preds
+
+
+def build_model(train_examples, dev_examples, pretrained_model, n_epochs=10, curr_model_path="temp.pt"):
+    global tokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+    encoder = AutoModel.from_pretrained(pretrained_model)
+    classifier = torch.nn.Linear(encoder.config.hidden_size, 1 if len(label_list) == 2 else len(label_list))
+
+    train_dataset = TransformersData(train_examples, label_list, tokenizer, max_seq_length=max_seq_length, has_token_type_ids=has_token_type_ids)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    dev_dataset = TransformersData(dev_examples, label_list, tokenizer, max_seq_length=max_seq_length, has_token_type_ids=has_token_type_ids)
+    dev_dataloader = DataLoader(dataset=dev_dataset, batch_size=batch_size)
+
+
+    classifier.to(device)
+    if torch.cuda.device_count() > 1 and device.type == "cuda":
+        encoder = torch.nn.DataParallel(encoder, device_ids=device_ids)
+    encoder.to(device)
+
+    optimizer = torch.optim.AdamW(list(classifier.parameters()) + list(encoder.parameters()), lr=learning_rate)
+    num_train_steps = int(len(train_examples) / batch_size * num_epochs)
+    # scheduler = get_linear_schedule_with_warmup(optimizer,
+    #                                             num_warmup_steps = 0,
+    #                                             num_training_steps = num_train_steps)
+
+    best_score = -1e6
+    for epoch in range(n_epochs):
+        start_time = time.time()
+        train_loss = 0
+        encoder.train()
+        classifier.train()
+
+        print("Starting Epoch %d"%(epoch+1))
+        global_step = 0
+        train_loss = 0.0
+        nb_tr_steps = 0
+        for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
+            if has_token_type_ids:
+                input_ids, input_mask, token_type_ids, label_ids = tuple(t.to(device) for t in batch)
+                embeddings = encoder(input_ids, attention_mask=input_mask, token_type_ids=token_type_ids)[1]
+            else:
+                input_ids, input_mask, label_ids = tuple(t.to(device) for t in batch)
+                embeddings = encoder(input_ids, attention_mask=input_mask)[1]
+
+            out = classifier(embeddings)
+            loss = criterion(out, label_ids.view(-1))
+
+            loss.backward()
+            global_step += 1
+            nb_tr_steps += 1
+
+            torch.nn.utils.clip_grad_norm_(encoder.parameters(), 1.0)
+            torch.nn.utils.clip_grad_norm_(classifier.parameters(), 1.0)
+            optimizer.step()
+            # scheduler.step()
+            encoder.zero_grad()
+            classifier.zero_grad()
+
+            train_loss += loss.item()
+
+        train_loss /= nb_tr_steps
+        elapsed = time.time() - start_time
+
+        # Validation
+        encoder.eval()
+        classifier.eval()
+        result, val_loss = test_model(encoder, classifier, dev_dataloader)
+        result["train_loss"] = train_loss
+        result["dev_loss"] = val_loss
+        result["elapsed"] = elapsed
+        print("***** Epoch " + str(epoch + 1) + " *****")
+        for key in sorted(result.keys()):
+            print("  %s = %.6f" %(key, result[key]))
+
+        print("Val score: %.6f" %result[dev_metric])
+
+        if result[dev_metric] > best_score:
+            print("Saving model!")
+            torch.save(classifier.state_dict(), repo_path + "/models/classifier_" + curr_model_path)
+            # encoder_to_save = encoder.module if hasattr(encoder, 'module') else encoder  # To handle multi gpu
+            torch.save(encoder.state_dict(), repo_path + "/models/encoder_" + curr_model_path)
+            best_score = result[dev_metric]
+
+        print("========================================================================")
+
+    return encoder, classifier
+
+if __name__ == '__main__':
+    if dev_set_splitting == "random":
+        train_examples = get_examples(train_filename)
+        random.shuffle(train_examples)
+        dev_split = int(len(train_examples) * dev_ratio)
+        dev_examples = train_examples[:dev_split]
+        train_examples = train_examples[dev_split:]
+    else: # it's a custom filename
+        train_examples = get_examples(train_filename)
+        random.shuffle(train_examples)
+        dev_examples = get_examples(dev_set_splitting)
+
+    if not only_test:
+        encoder, classifier = build_model(train_examples, dev_examples, pretrained_transformers_model, n_epochs=num_epochs, curr_model_path=model_path)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(pretrained_transformers_model)
+        encoder = AutoModel.from_pretrained(pretrained_transformers_model)
+        classifier = torch.nn.Linear(encoder.config.hidden_size, 1 if len(label_list) == 2 else len(label_list))
+        if torch.cuda.device_count() > 1 and device.type == "cuda":
+            encoder = torch.nn.DataParallel(encoder, device_ids=device_ids)
+
+    classifier.load_state_dict(torch.load(repo_path + "/models/classifier_" + model_path))
+    classifier.to(device)
+    encoder.load_state_dict(torch.load(repo_path + "/models/encoder_" + model_path))
+    encoder.to(device)
+    encoder.eval()
+    classifier.eval()
+
+    if predict:
+        test_examples = get_examples(test_filename, with_label=False)
+        test_dataset = TransformersData(test_examples, label_list, tokenizer, max_seq_length=max_seq_length, has_token_type_ids=has_token_type_ids, with_label=False)
+        test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size)
+
+        all_preds = model_predict(encoder, classifier, test_dataloader)
+        with open(test_filename, "r", encoding="utf-8") as f:
+            test = [json.loads(line) for line in f.read().splitlines()]
+
+        with open(repo_path + "/out.json", "w", encoding="utf-8") as g:
+            for i, t in enumerate(test):
+                t["prediction"] = all_preds[i]
+                g.write(json.dumps(t) + "\n")
+
+    else:
+        test_examples = get_examples(test_filename)
+        test_dataset = TransformersData(test_examples, label_list, tokenizer, max_seq_length=max_seq_length, has_token_type_ids=has_token_type_ids)
+        test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size)
+
+        result, test_loss = test_model(encoder, classifier, test_dataloader)
+        result["test_loss"] = test_loss
+
+        print("***** TEST RESULTS *****")
+        for key in sorted(result.keys()):
+            print("  %s = %.6f" %(key, result[key]))
+
+        print("TEST SCORE: %.6f" %result[dev_metric])
