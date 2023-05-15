729733bb122dee82c18ba5beb1505d178415a21a
Osman Mutlu <osmanmutlu92@gmail.com>
2023-05-15 18:08:48 +0300

    src/use_trained_model: small fix in bulk_write

--- src/use_trained_model.py
+++ src/use_trained_model.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
                                    max_length=max_seq_length)
                 preds = model_predict(inputs)
 
-                curr_updates = [UpdateOne({"_id": curr_batch[pred_idx]}, {"$set": {task_name: pred}}) for pred_idx, pred in enumerate(preds)]
+                curr_updates = [UpdateOne({"_id": curr_batch[pred_idx]["_id"]}, {"$set": {task_name: pred}}) for pred_idx, pred in enumerate(preds)]
                 tweet_col.bulk_write(curr_updates, ordered=False)
 
                 curr_batch = []
@@ -130,7 +130,7 @@ if __name__ == "__main__":
                                max_length=max_seq_length)
             preds = model_predict(inputs)
 
-            curr_updates = [UpdateOne({"_id": curr_batch[pred_idx]}, {"$set": {task_name: pred}}) for pred_idx, pred in enumerate(preds)]
+            curr_updates = [UpdateOne({"_id": curr_batch[pred_idx]["_id"]}, {"$set": {task_name: pred}}) for pred_idx, pred in enumerate(preds)]
             tweet_col.bulk_write(curr_updates, ordered=False)
 
 
